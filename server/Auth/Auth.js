7d4da99ff57f18e79f9c2ebaf5e081dffb06214d
kefilwe99 <wendykefilwe56@gmail.com>
2023-11-29 14:35:36 +0200

    removed node_modules

--- server/Auth/Auth.js
+++ server/Auth/Auth.js
@@ -1,57 +1,66 @@
 const User = require("../model/user")
+const bcrypt = require("bcryptjs")
 
 // auth.js
 exports.register = async (req, res, next) => {
-    const { email, password } = req.body
-    if (password.length < 6) {
-      return res.status(400).json({ message: "Password less than 6 characters" })
-    }
-    try {
-      await User.create({
-        email,
-        password,
-      }).then(user =>
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then(async (hash) => {
+    await User.create({
+      username: "wendykefilwe56@gmail.com",
+      password: hash,
+    })
+      .then((user) =>
         res.status(200).json({
           message: "User successfully created",
-          user,
+           user,
         })
       )
-    } catch (err) {
-      res.status(401).json({
-        message: "User not successful created",
-        error: error.mesage,
-      })
-    }
-  }
+      .catch((error) =>
+        res.status(400).json({
+          message: "User not successful created",
+          error: error.message,
+        })
+      );
+  });
+};
+
+
+
+
   // auth.js
 exports.login = async (req, res, next) => {
     const { email, password } = req.body
     // Check if username and password is provided
-    if (!email || !password) {
+    if (!username || !password) {
       return res.status(400).json({
         message: "Username or Password not present",
       })
     }
-    try {
-        const user = await User.findOne({ email, password })
-        if (!user) {
-          res.status(401).json({
-            message: "Login not successful",
-            error: "User not found",
-          })
-        } else {
-          res.status(200).json({
-            message: "Login successful",
-            user,
-          })
-        }
-      } catch (error) {
-        res.status(400).json({
-          message: "An error occurred",
-          error: error.message,
-        })
-      }
-      //auth.js
+      try {
+    const user = await User.findOne({ username, password })
+    if (!user) {
+      res.status(401).json({
+        message: "Login not successful",
+        error: "User not found",
+      })
+    } else {
+      res.status(200).json({
+        message: "Login successful",
+        user,
+      })
+    }
+  } catch (error) {
+    res.status(400).json({
+      message: "An error occurred",
+      error: error.message,
+    })
+  }
+  
+}
+  //auth.js
+
+
+
 exports.update = async (req, res, next) => {
     const { role, id } = req.body
     // Verifying if role and id is presnt
@@ -68,4 +77,6 @@ exports.update = async (req, res, next) => {
       res.status(400).json({ message: "Role or Id not present" })
     }
   }
-  }
+
+
+  
--- server/Auth/Route.js
+++ server/Auth/Route.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const router = express.Router()
-const { register, login } = require("./Auth")
+const { register, login, update } = require("./Auth")
+
+
+
 router.route("/register").post(register)
 router.route("/login").post(login);
-const { register, login, update } = require("./Auth");
 router.route("/update").put(update);
+
+
 module.exports = router;
