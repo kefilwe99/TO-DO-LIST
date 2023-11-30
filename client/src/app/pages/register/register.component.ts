14999abbba1d21021ccb0550f8b2522d150ffac0
kefilwe99 <wendykefilwe56@gmail.com>
2023-11-30 16:17:31 +0200

    register changes

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { User } from 'src/app/interfaces/user';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +10,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
 
+  items: User[] = [];
+  constructor(
+    private service:UsersService,
+    private router: Router
+  ) {}
+
+  form: FormGroup = new FormGroup({
+    name: new FormGroup(''),
+    email: new FormGroup(''),
+    password: new FormGroup(''),
+  });
+        
+  ngOnIit(): void {}
+
+  onSubmit() {
+    if (!this.name || !this.email || !this.password) {
+       alert('All fileds are required');
+      return;
+    }
+  
+    const object = {
+      name: this.name,
+      email: this.email,
+      password: this.password,
+    };
+    this.service.registerFn(object).subscribe(
+      (res: any) => {
+        alert(res.message);
+        this.router.navigate(['/home']);
+      },
+      (error) => {
+        alert(error.message + '  ' + error.error._message);
+       
+      }
+    );
+  }
 }
